b1b96479c0d7b93e35f08e4d2013d034aec0783b
HCH270 <82739685+HCH270@users.noreply.github.com>
2021-05-04 15:04:28 -0500

    Revert "Course Project - Directives"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './sharing/dropdown.directive';
-
 
 @NgModule({
   declarations: [
@@ -22,9 +20,7 @@ import { DropdownDirective } from './sharing/dropdown.directive';
     ShoppingListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    ShoppingEditComponent,
-    DropdownDirective
-
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
