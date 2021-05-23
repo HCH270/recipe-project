84995ce06dd85e9164fe59cf49b874f7491e7018
HCH270 <hchawkins@gmail.com>
2021-05-23 15:30:24 -0500

    Comparing to Max's
    My recipe description button aren't showing when they are "activated" or selected

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,15 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import { Recipe } from './recipe.model';
+
 
 @Injectable()
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe (
+    new Recipe(
       'Tasty Schnitzel',
       'A super-tasty Schnitzel!',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAc3WBlH5FxrprrBj6gT3tZgHwFGMu4XycqQ&usqp=CAU',
@@ -16,7 +17,7 @@ recipeSelected = new EventEmitter<Recipe>();
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
-    new Recipe (
+    new Recipe(
      'Big Fat Burger',
      'What else do you need to say?',
      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKhdJ6Md_TVBcp3oNVjk-uyI34eI_D4Oo64g&usqp=CAU',
@@ -24,7 +25,7 @@ recipeSelected = new EventEmitter<Recipe>();
        new Ingredient('Buns', 2),
        new Ingredient('Meat', 1)
      ]),
-     new Recipe (
+     new Recipe(
       'Yummy Egg Pie',
       'Perfect Breakfast!',
       'https://cdn.pixabay.com/photo/2015/05/23/22/04/omelette-781105_1280.jpg',
