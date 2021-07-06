97cf43f42e3edefbda7c88941ce15fe4bf04326d
HCH270 <hchawkins@gmail.com>
2021-07-06 13:23:08 -0500

    Dynamic Components*
    *not sure this all belongs on this lesson.  I've lost my place since vacation and hadn't committed this.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -18,7 +18,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
-import { AuthComponent } from './auth.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -33,7 +37,10 @@ import { AuthComponent } from './auth.component';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
-    AuthComponent
+    AuthComponent,
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
 
   ],
   imports: [
@@ -46,7 +53,10 @@ import { AuthComponent } from './auth.component';
 
 
   ],
-  providers: [ShoppingListService, RecipeService],
-  bootstrap: [AppComponent]
+  providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
+  bootstrap: [AppComponent],
+  entryComponents: [
+   AlertComponent
+  ]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap} from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap} from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
 storeRecipes() {
   const recipes = this.recipeService.getRecipes();
@@ -23,21 +24,22 @@ storeRecipes() {
 
 fetchRecipes() {
   return this.http
-  .get<Recipe[]>('https://this-project-1fd1e-default-rtdb.firebaseio.com/recipes.json'
-  )
-  .pipe(
+    .get<Recipe[]>(
+        'https://this-project-1fd1e-default-rtdb.firebaseio.com/recipes.json',
+       )
+    .pipe(
     map(recipes => {
-    return recipes.map(recipe => {
-      return {
-        ...recipe,
-        ingredients: recipe.ingredients ? recipe.ingredients : []
-      };
-    });
-  }),
-    tap(recipes => {
-      this.recipeService.setRecipes(recipes);
-    })
-  )
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 }
 
