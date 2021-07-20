7e46428707472f2281d85a3bfc806f82a5534eb9
HCH270 <hchawkins@gmail.com>
2021-07-20 13:30:52 -0500

    DeployApp&InstallExpressServer/Heroku
    This was Max's Deployment lesson. he continued using firebase for static server in addition to using it for backend previously. I installed express server and will deploy to Heroku.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyBj-yeDwHW7h_YTFGNKDfR3WpUwrZC-m6U'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyBj-yeDwHW7h_YTFGNKDfR3WpUwrZC-m6U'
 };
 
 /*
